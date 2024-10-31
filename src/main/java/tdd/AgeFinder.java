61ba71d1930c6538beb1273fe86b00f075e036fd
kalki <kalki_qa_lbg@gmaill.com>
2024-10-31 12:00:11 +0000

    test1

--- src/main/java/tdd/AgeFinder.java
+++ src/main/java/tdd/AgeFinder.java
@@ -1,4 +1,56 @@
 package tdd;
 
 public class AgeFinder {
+
+    //fn to extract the digits
+
+    public int findDigits(String input){
+        if(input == null || input.length() == 0) {
+            throw new IllegalArgumentException("Empty license string");
+        }
+
+        //Find the 2 digits
+        String ageId = input.substring(2,4);
+        int numAgeId = Integer.parseInt(ageId);
+        return numAgeId;
+    }
+
+    //fn to fnd next age id
+    public String findNextAge(String input) {
+
+        int numAgeId = findDigits(input);
+        String numStr = Integer.toString(numAgeId);
+        if(numAgeId >=0 && numAgeId <=9){
+            numStr = "0" + numStr;
+        }
+        if(numStr.length() != 2){
+            throw new IllegalArgumentException("No 2 digits");
+        }
+
+        String result = "";
+
+        if(numAgeId > 50){
+            result = Integer.toString(numAgeId - 49);
+            if(result.length()==1){
+                result = "0" + result;
+            }
+            return result;
+        }
+
+        else if(numAgeId == 50){
+            return "00";
+        }
+
+        else if(numAgeId < 50){
+            result = Integer.toString(numAgeId + 50);
+            return result;
+        }
+
+
+        else{
+            throw new IllegalArgumentException("Not valid id");
+        }
+
+
+    }
 }
--- src/test/java/tdd/AgeFinderTest.java
+++ src/test/java/tdd/AgeFinderTest.java
@@ -2,7 +2,75 @@ package tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AgeFinderTest {
 
+
     @Test
+    public void verify_2_digits_in_input(){
+        String input = "GW05MXZ";
+        AgeFinder cut = new AgeFinder();
+        String expectedResult = "55";
+
+        String actualResult = cut.findNextAge(input);
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+//    @Test
+//    public void verify_empty_input(){
+//        String input = "";
+//        AgeFinder cut = new AgeFinder();
+//
+//        Exception e = assertThrows(IllegalArgumentException.class,() -> {
+//            cut.findNextAge(input);
+//        });
+//
+//        assertEquals("Input must not be empty",e.getMessage());
+//
+//    }
+//    @Test
+//    public void verify_if_input_is_in_first_half_of_the_year(){
+//        String input = "GW05MXZ";
+//        AgeFinder cut = new AgeFinder();
+//        String expectedResult = "55";
+//
+//        String actualResult = cut.findNextAge(input);
+//
+//        assertEquals(expectedResult,actualResult);
+//    }
+//
+//    @Test
+//    public void verify_if_input_is_in_second_half_of_the_year(){
+//        String input = "GW55MXZ";
+//        AgeFinder cut = new AgeFinder();
+//        String expectedResult = "06";
+//
+//        String actualResult = cut.findNextAge(input);
+//
+//        assertEquals(expectedResult,actualResult);
+//    }
+//
+//
+//    @Test
+//    public void verify_when_input_is_50(){
+//
+//        String input = "GW50MXZ";
+//        AgeFinder cut = new AgeFinder();
+//        String expectedResult = "00";
+//
+//        String actualResult = cut.findNextAge(input);
+//
+//        assertEquals(expectedResult,actualResult);
+//
+//    }
+//
+//    @Test
+//    public void verify_when_input_is_00(){
+//
+//    }
+
+
 }
